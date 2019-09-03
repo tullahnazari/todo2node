73fa5fedc011255760b390b0ed26191ffc7e2bf8
tullahnazari <tullah.nazari@gmail.com>
2019-09-03 15:13:24 -0500

    refactored by creating routes folder

--- app.js
+++ app.js
@@ -5,46 +5,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true });
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-bookRouter.route('/books')
-//k ,making query string param
-  .post((req, res) => {
-    const book = new Book(req.body);
-//saving post responses to book 
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
 
-  bookRouter.route('/books/:bookId')
-//find book by id
-  .get((req, res) => {
-    
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
---
+++ routes/bookRouter.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+function routes(Book){
+  const bookRouter = express.Router();
+  bookRouter.route('/books')
+//making query string param
+  .post((req, res) => {
+    const book = new Book(req.body);
+//saving post responses to book 
+    book.save();
+    return res.status(201).json(book);
+  })
+  .get((req, res) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  });
+
+  bookRouter.route('/books/:bookId')
+//find book by id
+  .get((req, res) => {
+    
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(book);
+    });
+  });
+
+  return bookRouter;
+}
+
+module.exports = routes;
